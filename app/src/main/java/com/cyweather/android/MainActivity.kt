1e4fc22570ffc3a5ca7ba4be4d971c6328e06375
shirwee <xuanwei.xu@chetailian.com>
2021-04-08 15:24:58 +0800

    icon res

--- app/src/main/java/com/cyweather/android/MainActivity.kt
+++ app/src/main/java/com/cyweather/android/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.cyweather.android
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.edit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
